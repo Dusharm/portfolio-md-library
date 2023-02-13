fb33ea816c0381a596fc72d1f0411e1d86753316
Daniel Dusharm <djdusharm@outlook.com>
2023-02-13 06:00:42 -0500

    script cleanup

--- README.md
+++ README.md
@@ -2,5 +2,20 @@
 
 ## Collection of mdBooks for my portfolio
 
+Just a repository to help me organize the books I write for my own web pages.
 
-Just a repository to help me organize the books I write for my own web pages
+<br>
+
+# Build tools
+
+Run scripts from root of repo.
+
+## Build books
+Run from root of repo. Builds all of the books.
+
+` ./build/build.sh `
+
+## Package books
+Packages books by name into build/target with a text file with relative links
+
+`./build/package.sh`
---
+++ build/function/build_fn.sh
@@ -0,0 +1,41 @@
+function buildBooks() {
+# Find book directories by searching for book.toml
+# run from project root
+
+    BOOKLIST="$(find . -type f -name "book.toml" -exec realpath {} \; )"
+
+    if [[ $BOOKLIST == "" ]]; then
+        echo Found no books!
+        exit 2
+    fi
+
+    echo Found the following books:
+    echo ---------------------------
+    for BOOK in $BOOKLIST; do
+        echo $( echo $BOOK | sed -e s~/book.toml~~g -e s~/.*/~~g)
+    done
+    echo
+
+    for BOOK in $BOOKLIST; do
+        BOOK_DIR=$( echo $BOOK | sed -e s~/book.toml~~g  )
+        buildBook $BOOK_DIR
+    done
+}
+
+function buildBook(){
+    # run mdBook from path in $1
+
+    BOOK_DIR=$1
+
+    cd $BOOK_DIR
+    echo Building from $BOOK_DIR
+    echo ---------------------------
+
+    # Can't seem to redirect output for this command. Generation
+    # is very fast so it's not very worth to put this to background
+    # in case of failure
+    mdbook build
+
+    echo Done $BOOK_DIR
+    echo
+}
